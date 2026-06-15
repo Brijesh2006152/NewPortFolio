963ac789327b341b360aef91f0faa3ec7f7e0fc2
Brijesh2006152 <brijeshpatel2006152@gmail.com>
2026-06-15 12:00:35 +0530

    skill refactored into list format

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
 const SkillSection = (props) => {
+  
   return (
     <section className='skill-set'>
         <div className="skill-set-head">{props.head}</div>
+        <ul>
+          {
+            props.body.map((skill) => (
+              <li key={skill.id}>{skill}</li>
+            ))
+          }
+        </ul>
+        {/*
         <div className="skill-set-body"> {props.body}</div>
+        */}
     </section>
   )
 }
 
-export default SkillSection
+export default SkillSection;
